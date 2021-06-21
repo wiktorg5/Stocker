228f4b3efb7e3c9e59e6f01df4b8e68038bfe263
wiktorg5 <83000103+wiktorg5@users.noreply.github.com>
2021-06-21 19:25:13 +0200

    chart multi datasets
    Added functionality to the chart so now it can present many datasets.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import './css/App.css';
-import Chart from "./main_components/Chart"
-import Logo from "./main_components/Logo"
-import Comparison from "./main_components/Comparison"
-import Footer from "./main_components/Footer"
-import Navbar from "./main_components/Navbar"
+import Chart from './main_components/Chart';
+import Logo from './main_components/Logo';
+import Footer from './main_components/Footer';
+import Navbar from './main_components/Navbar';
 
 function App() {
-
-  return<>
-    <Navbar />
-    <Logo />
-    <Chart />
-    <Comparison />
-    <Footer />
-  </>
+	return (
+		<>
+			<Navbar />
+			<Logo />
+			<Chart />
+			<Footer />
+		</>
+	);
 }
-
-
-
 
 export default App;
--- src/main_components/Chart.js
+++ src/main_components/Chart.js
@@ -1,116 +1,150 @@
 import React from 'react';
-import { useState } from 'react/cjs/react.development';
+import { useState, useEffect } from 'react/cjs/react.development';
 import '../css/App.css';
-import {Line} from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 
 const Chart = () => {
+	//overview and symbol states
+	const [symbol, setSymbol] = useState('');
+	const [overview, setOverview] = useState({
+		Name: '',
+		Exchange: '',
+		Currency: '',
+	});
 
-    const [symbol, setSymbol] = useState('');
-    const [overview, setOverview] = useState({
-        Name:'',
-        Exchange:'',
-        Currency:''
-    })
-    let linkDaily = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=YJ0EZ107WVSIQNS1`;
-    let linkOverview = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=YJ0EZ107WVSIQNS1`;
+	//links
+	let linkDaily = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=YJ0EZ107WVSIQNS1`;
+	let linkOverview = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=YJ0EZ107WVSIQNS1`;
 
-    let [mappedPrice, setMappedPrice] = useState([]);
-    let [mappedDate, setMappedDate] = useState([]) ;
-    
-    const [sumbitted, setSubmitted] = useState(false);
+	//chart data states
+	const [mappedPrice, setMappedPrice] = useState([]);
+	const [mappedDate, setMappedDate] = useState([]);
 
-    const getSymbol = (e) =>{
-        let symbolInput = e.target.value.toUpperCase();
-        setSymbol(symbolInput);
-    }
+	//chart state
+	const [states, setStates] = useState({
+		labels: mappedDate,
+		datasets: mappedPrice,
+	});
 
-    async function getData (e){
-        e.preventDefault();
-        try{
-            const data = await fetch(linkDaily);
-            const daily = await data.json();
-        
-        let fetchedDaily = daily['Time Series (Daily)'];  
-        let arrayFetchedDaily = Object.entries(fetchedDaily);
-        
-        setMappedPrice(arrayFetchedDaily.map((item)=>{
-            return parseFloat(item[1]['4. close']);
-        }).reverse())
+	//if form submitted state
+	const [submitted, setSubmitted] = useState(false);
 
-        setMappedDate(arrayFetchedDaily.map((item)=>{
-            return item[0];
-        }).reverse())
+	//getting symbol from input
+	const getSymbol = (e) => {
+		let symbolInput = e.target.value.toUpperCase();
+		setSymbol(symbolInput);
+	};
 
+	//changing chart data
+	useEffect(() => {
+		setStates({ labels: mappedDate, datasets: mappedPrice });
+		return () => {};
+	}, [mappedPrice, mappedDate]);
 
-        const overview = await fetch(linkOverview);
-        const fetchedOverview = await overview.json();
+	//random colors
+	let value1 = Math.floor(Math.random() * 170);
+	let value2 = Math.floor(Math.random() * 120);
+	let value3 = Math.floor(Math.random() * 78);
 
+	//taking data from json and making chart
+	async function getData(e) {
+		e.preventDefault();
+		try {
+			//getting daily data
+			const data = await fetch(linkDaily);
+			const daily = await data.json();
 
-        const {Name, Exchange, Currency} = fetchedOverview;
-        console.log(Name, Exchange, Currency);
-        setOverview({Name,Exchange,Currency})
+			let fetchedDaily = daily['Time Series (Daily)'];
+			let arrayFetchedDaily = Object.entries(fetchedDaily);
 
-        setSubmitted(true);
-        }catch(err){
-            console.log(err);
-        }
-        
-        return {};
-    }
+			//mapping prices and creating datasets for chart
+			await setMappedPrice([
+				...mappedPrice,
+				{
+					label: symbol,
+					fill: false,
+					lineTension: 0.5,
+					backgroundColor: `rgba(${value1},${value2},${value3},1)`,
+					borderColor: 'rgba(0,0,0,1)',
+					borderWidth: 2,
+					data: arrayFetchedDaily
+						.map((item) => {
+							return parseFloat(item[1]['4. close']);
+						})
+						.reverse(),
+				},
+			]);
 
-    
-    let state = {
-        labels: mappedDate,
-        datasets: [
-            {
-            label: 'Price',
-            fill: false,
-            lineTension: 0.5,
-            backgroundColor: 'rgba(75,192,192,1)',
-            borderColor: 'rgba(0,0,0,1)',
-            borderWidth: 2,
-            data: mappedPrice
-            }
-        ]
-        }
-    
+			//mapping time
+			setMappedDate(
+				arrayFetchedDaily
+					.map((item) => {
+						return item[0];
+					})
+					.reverse()
+			);
 
-    return (
-        <div className="Chart">
-            <div className="MainChart">
-            <div className="choose">
-                <form className="form_chart" onSubmit={getData}>
-                    <input type="text" className="symbol" value={symbol} onChange={getSymbol} placeholder="Symbol.."></input>
-                    <button type="submit" className="btn">Search</button>
-                </form>
-            </div>
-            <div className="chart">
-                    {
-                        
-                       
-                    sumbitted && <Line data={state} options={{
-                    title:{
-                    display:true,
-                    text:'Stock price',
-                    fontSize:40
-                    },
-                    legend:{
-                    display:true,
-                    position:'right'
-                    }
-                }} />
-            }
-                
-            </div>
-            <div className="description">
-                <span><span className="prefix">Company Name: </span>{overview.Name}</span>
-                <span><span className="prefix">Exchange: </span>{overview.Exchange}</span>
-                <span><span className="prefix">Currency: </span>{overview.Currency}</span>
-            </div>
-            <button className="to_comparison">Add to Comparison</button>
-            </div>
-        </div>
-    )
-}
+			//fetching overview
+			const overview = await fetch(linkOverview);
+			const fetchedOverview = await overview.json();
 
-export default Chart
+			const { Name, Exchange, Currency } = fetchedOverview;
+			console.log(Name, Exchange, Currency);
+			setOverview({ Name, Exchange, Currency });
+
+			setSubmitted(true);
+		} catch (err) {
+			console.log(err);
+		}
+
+		return {};
+	}
+
+	return (
+		<>
+			<div className='Chart'>
+				<div className='MainChart'>
+					<div className='description'></div>
+
+					<div className='chart'>
+						{submitted && (
+							<Line
+								data={states}
+								options={{
+									title: {
+										display: true,
+										text: 'Stock price',
+										fontSize: 40,
+									},
+									legend: {
+										display: true,
+										position: 'right',
+									},
+								}}
+							/>
+						)}
+					</div>
+					<div className='choose'>
+						<form className='form_chart' onSubmit={getData}>
+							<input
+								type='text'
+								className='symbol'
+								value={symbol}
+								onChange={getSymbol}
+								placeholder='Symbol..'
+							></input>
+							<button type='submit' className='btn'>
+								Search
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+			<div className='Comparison'>
+				<div className='MainComparison'></div>
+			</div>
+		</>
+	);
+};
+
+export default Chart;

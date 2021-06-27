d5d03e8ccbb6b4cff4226ddcfad8093b99420b47
wiktorg5 <83000103+wiktorg5@users.noreply.github.com>
2021-06-27 21:43:28 +0200

    Added basic articles styling, finish work with CMS

--- src/main_components/News.js
+++ src/main_components/News.js
@@ -8,17 +8,32 @@ const News = () => {
 	const client = Prismic.client(apiEndpoint);
 
 	const [objects, setObjects] = useState(null);
+	const [display, setDisplay] = useState(null);
 
 	const makeObjects = async (data) => {
 		const objecty = data.map((item) => {
 			const object = {
 				title: item.data.title[0]['text'],
+				text: item.data.text[0]['text'],
 			};
 
 			return object;
 		});
 		setObjects(objecty);
 		return objects;
+	};
+
+	const displayData = () => {
+		setDisplay(
+			objects.map((item) => {
+				return (
+					<article className='news_article'>
+						<header className='news_article_header'>{item.title}</header>
+						<section className='news_article_text'>{item.text}</section>
+					</article>
+				);
+			})
+		);
 	};
 
 	const fetchData = async () => {
@@ -34,11 +49,13 @@ const News = () => {
 
 	useEffect(() => {
 		fetchData();
-	}, []);
+		objects && displayData();
+	}, [objects]);
 
 	return (
 		<div className='News'>
-			<div className='Main_News'>{objects && objects[0]['title']}</div>
+			<span className='articles_header'>News</span>
+			{display}
 		</div>
 	);
 };

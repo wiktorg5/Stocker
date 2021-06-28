1d08af328d0e52b7b3de9805486e03a2a8779977
wiktorg5 <83000103+wiktorg5@users.noreply.github.com>
2021-06-28 17:20:53 +0200

    footer icons colors

--- src/main_components/Footer.js
+++ src/main_components/Footer.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import '../css/App.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-
+import React from 'react';
+import '../css/App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faFacebook,
+	faInstagram,
+	faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
-    return (
-        <div className="Footer">
-            <FontAwesomeIcon icon={faFacebook} size="5x" />
-            <FontAwesomeIcon icon={faInstagram} size="5x" />
-            <FontAwesomeIcon icon={faLinkedin} size="5x" />
-        </div>
-    )
-}
+	return (
+		<div className='Footer'>
+			<FontAwesomeIcon className='fb_icon' icon={faFacebook} size='5x' />
+			<FontAwesomeIcon className='ig_icon' icon={faInstagram} size='5x' />
+			<FontAwesomeIcon className='li_icon' icon={faLinkedin} size='5x' />
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
